b36c47b89c3b70bac018ff59883aa8c65afe37e9
samuelkwik <samuelkwik@ymail.com>
2025-03-16 22:04:43 +0800

    Fix merge conflict error

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -274,7 +274,7 @@ public class EditCommand extends Command {
                     && Objects.equals(email, otherEditPersonDescriptor.email)
                     && Objects.equals(address, otherEditPersonDescriptor.address)
                     && Objects.equals(tags, otherEditPersonDescriptor.tags)
-                    && Objects.equals(tagsToRemove, otherEditPersonDescriptor.tagsToRemove);
+                    && Objects.equals(tagsToRemove, otherEditPersonDescriptor.tagsToRemove)
                     && Objects.equals(currentGrade, otherEditPersonDescriptor.currentGrade);
         }
 
