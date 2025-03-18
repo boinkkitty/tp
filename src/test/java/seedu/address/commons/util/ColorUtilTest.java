cd724eab39aa925b21528fe3b9d883e94dc626d2
JunXiang26 <60207430+JunXiang26@users.noreply.github.com>
2025-03-18 19:01:38 +0800

    Add colors to current grade feature

--- src/test/java/seedu/address/commons/util/ColorUtilTest.java
+++ src/test/java/seedu/address/commons/util/ColorUtilTest.java
@@ -1,7 +1,9 @@
 package seedu.address.commons.util;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.commons.util.ColorUtil.getGradeHexColor;
 import static seedu.address.commons.util.ColorUtil.isLightColor;
 
 import org.junit.jupiter.api.Test;
@@ -20,5 +22,16 @@ public class ColorUtilTest {
     @Test
     public void isLightColor_invalidHexCode_returnTrue() {
         assertTrue(isLightColor("INVALID_HEX_COLOR"));
+    }
+
+    @Test
+    public void getHexColor() {
+        assertEquals(getGradeHexColor("A"), "#6DF162");
+        assertEquals(getGradeHexColor("B"), "#D2F088");
+        assertEquals(getGradeHexColor("C"), "#E1E888");
+        assertEquals(getGradeHexColor("D"), "#E8D588");
+        assertEquals(getGradeHexColor("E"), "#EE9C59");
+        assertEquals(getGradeHexColor("F"), "#EC736E");
+        assertEquals(getGradeHexColor(""), "#FFFFFF");
     }
 }
