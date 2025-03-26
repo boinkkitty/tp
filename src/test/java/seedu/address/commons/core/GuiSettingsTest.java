3de065415793001a62174788b88bcd1d98e03824
JunXiang26 <60207430+JunXiang26@users.noreply.github.com>
2025-03-26 22:01:24 +0800

    Update for CodeCov

--- src/test/java/seedu/address/commons/core/GuiSettingsTest.java
+++ src/test/java/seedu/address/commons/core/GuiSettingsTest.java
@@ -6,6 +6,16 @@ import org.junit.jupiter.api.Test;
 
 public class GuiSettingsTest {
     @Test
+    public void testGuiSettings() {
+        GuiSettings guiSettings = new GuiSettings();
+
+        assertEquals(guiSettings.getWindowWidth(), 740);
+        assertEquals(guiSettings.getWindowHeight(), 600);
+        assertEquals(guiSettings.getWindowCoordinates(), null);
+        assertEquals(guiSettings.getIsDarkTheme(), true);
+    }
+
+    @Test
     public void toStringMethod() {
         GuiSettings guiSettings = new GuiSettings();
         String expected = GuiSettings.class.getCanonicalName() + "{windowWidth=" + guiSettings.getWindowWidth()
